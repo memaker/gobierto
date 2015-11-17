dd101b21e4515471ffe94ea80ac29ea430ecd46b
Fernando Blat <ferblape@gmail.com>
2015-11-17 11:29:53 +0100

    Compare with similar population

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,14 @@ module ApplicationHelper
       end.join("\n")}
     </ul>".html_safe
   end
+
+  def similar_population_parameters(population)
+    r = BudgetFilter.populations.map do |filter|
+      array = filter.first.split(' - ').map{|s| s.tr('.','').to_i }
+      array[1] = 100_000_000 if array[1] == 0
+      Range.new *array
+    end.detect{|r| r.include?(population) }
+
+    {population: "#{r.first} - #{r.last}"}
+  end
 end
