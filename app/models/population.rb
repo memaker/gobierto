105bb3d72fdb2ab2d8a751e03877ced575e8985d
Fernando Blat <ferblape@gmail.com>
2015-11-17 16:35:02 +0100

    Render lists of bigger and smaller budgets

--- app/models/population.rb
+++ app/models/population.rb
@@ -16,4 +16,38 @@ class Population < ActiveRecord::Base
     ActiveRecord::Base.connection.execute(query).detect{|r| r['ine_code'].to_i == self.codigo }['rank'].to_i
   end
 
+  def bigger_functional_budgets(year)
+    sql = <<-SQL
+select importe as amount, "tb_cuentasProgramas".nombre as name
+FROM tb_funcional
+INNER join "tb_cuentasProgramas" ON "tb_cuentasProgramas".cdfgr = tb_funcional.cdfgr
+INNER JOIN poblacion_municipal_2014 ON poblacion_municipal_2014.codigo = tb_funcional.ine_code
+WHERE cdcta is null and ine_code = #{self.codigo} AND year = #{year}
+AND tb_funcional.level = 3
+ORDER BY importe DESC
+LIMIT 10
+SQL
+
+    ActiveRecord::Base.connection.execute(sql).map do |row|
+      BudgetLine.new row
+    end
+  end
+
+  def smaller_functional_budgets(year)
+    sql = <<-SQL
+select importe as amount, "tb_cuentasProgramas".nombre as name
+FROM tb_funcional
+INNER join "tb_cuentasProgramas" ON "tb_cuentasProgramas".cdfgr = tb_funcional.cdfgr
+INNER JOIN poblacion_municipal_2014 ON poblacion_municipal_2014.codigo = tb_funcional.ine_code
+WHERE cdcta is null and ine_code = #{self.codigo} AND year = #{year}
+AND tb_funcional.level = 3
+ORDER BY importe ASC
+LIMIT 10
+SQL
+
+    ActiveRecord::Base.connection.execute(sql).map do |row|
+      BudgetLine.new row
+    end
+
+  end
 end
