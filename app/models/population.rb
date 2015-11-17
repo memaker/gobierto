3cfe263c5cee1ccff02d023d034013ea2eeb3901
Fernando Blat <ferblape@gmail.com>
2015-11-17 15:42:14 +0100

    Calculate ranking

--- app/models/population.rb
+++ app/models/population.rb
@@ -6,4 +6,14 @@ class Population < ActiveRecord::Base
     @place ||= INE::Places::Place.find(self.codigo)
   end
 
+  def ranking(year, code = nil)
+    query = if code.nil?
+      "select codigo as ine_code,rank() OVER (ORDER BY total_functional_#{year} DESC) FROM poblacion_municipal_2014"
+    else
+      "select ine_code,rank() OVER (ORDER BY importe DESC) FROM tb_funcional WHERE (cdcta IS NULL AND year = #{year} AND cdfgr = '#{code}')"
+    end
+
+    results = ActiveRecord::Base.connection.execute(query).detect{|r| r['ine_code'].to_i == self.codigo }['rank']
+  end
+
 end
