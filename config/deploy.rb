a0ed000c097306aad83dcf67800ba74567cf009b
Fernando Blat <ferblape@gmail.com>
2015-11-19 15:53:48 +0100

    Shared link public/cache

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.0'
 set :application, 'gobierto-budgets-comparator'
 set :repo_url, 'git@github.com:PopulateTools/gobierto-budgets-comparator.git'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/cache')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.2.2'
 
