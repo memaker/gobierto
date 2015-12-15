4b586ffd5270d46f36bc8577ecc54ed2ea2b40f7
Jorge Gómez Sancha <jorge@populate.tools>
2015-12-15 17:15:27 +0100

    Using slugs instead of IDs

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   def show
-    @place = INE::Places::Place.find params[:id]
+    @place = INE::Places::Place.find_by_slug params[:id]
     @area_name = params[:area] || 'economic'
     @income_lines = BudgetLine.search(ine_code: @place.id, level: 1, year: 2015, kind: BudgetLine::INCOME, type: 'economic')
     @expense_lines = BudgetLine.search(ine_code: @place.id, level: 1, year: 2015, kind: BudgetLine::EXPENSE, type: @area_name)
