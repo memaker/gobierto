776c6ac037b98091b709823db927d739dd6eba4c
Jorge Gómez Sancha <jorge@populate.tools>
2015-12-15 16:02:58 +0100

    passing area as parameter

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,6 @@ class PlacesController < ApplicationController
   def show
     @place = INE::Places::Place.find params[:id]
     @income_lines = BudgetLine.search(ine_code: @place.id, level: 1, year: 2015, kind: BudgetLine::INCOME, type: 'economic')
-    @expense_lines = BudgetLine.search(ine_code: @place.id, level: 1, year: 2015, kind: BudgetLine::EXPENSE, type: 'economic')
+    @expense_lines = BudgetLine.search(ine_code: @place.id, level: 1, year: 2015, kind: BudgetLine::EXPENSE, type: (params[:area] || 'economic'))
   end
 end
