0bd596542b483df59531fd11ad2dcbb2da9da717
Jorge Gómez Sancha <jorge@populate.tools>
2015-12-17 12:50:33 +0100

    basic structure for expense/income pages

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,9 @@ Rails.application.routes.draw do
 
   ## New code
 
-  # resources :places
   get '/places/:slug/:year' => 'places#show', as: :place
+  get '/places/:slug/:year/expense' => 'places#expense', as: :place_expense
+  get '/places/:slug/:year/income' => 'places#income', as: :place_income
 
   namespace :api do
     get '/data/treemap/:ine_code/:type/:year/:kind/:level' => 'data#treemap', as: :data_treemap
