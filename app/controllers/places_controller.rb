6ac7d8d8813971f4c63eb80117c241fcffb15c19
Jorge Gómez Sancha <jorge@populate.tools>
2015-12-17 12:50:33 +0100

    basic structure for expense/income pages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,7 @@
 class PlacesController < ApplicationController
+  before_action :get_params
+  
   def show
-    @place = INE::Places::Place.find_by_slug params[:slug]
-    @area_name = params[:area] || 'economic'
-    @year = params[:year]
     @income_lines = BudgetLine.search(ine_code: @place.id, level: 1, year: @year, kind: BudgetLine::INCOME, type: 'economic')
     @expense_lines = BudgetLine.search(ine_code: @place.id, level: 1, year: @year, kind: BudgetLine::EXPENSE, type: @area_name)
 
@@ -11,5 +10,20 @@ class PlacesController < ApplicationController
       format.js
     end
 
+  end
+
+  def expense
+
+  end
+
+  def income
+
+  end
+
+  private
+  def get_params
+    @place = INE::Places::Place.find_by_slug params[:slug]
+    @area_name = params[:area] || 'economic'
+    @year = params[:year]
   end
 end
