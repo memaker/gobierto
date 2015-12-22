f1550bbf3c662d2517bf9767badfa0eeaaaf85a9
Fernando Blat <ferblape@gmail.com>
2015-12-22 17:09:46 +0100

    Add tooltip to treemap

--- app/assets/javascripts/vis_treemap.js
+++ app/assets/javascripts/vis_treemap.js
@@ -18,10 +18,19 @@ var TreemapVis = Class.extend({
 
     var colors = ['#FFD100', '#FE7000', '#ED2F00', '#940099', '#487304', '#4A73B0', '#1B4145', '#444300', '#24190E'];
     this.colorScale = d3.scale.ordinal().range(colors);
+
+    this.tooltip = null;
+    this.opacity = 1;
+    this.duration = 1;
   },
 
   render: function(urlData) {
     $(this.containerId).html('');
+
+    // Append tooltip
+    this.tooltip = d3.select('body').append('div')
+      .attr('class', 'treemap_tooltip')
+      .style('opacity', 0);
 
     // Chart dimensions
     this.containerWidth = parseInt(d3.select(this.containerId).style('width'), 10);
@@ -38,6 +47,9 @@ var TreemapVis = Class.extend({
     this.treemap = d3.layout.treemap()
       .size([this.width, this.height])
       .sticky(true)
+      .sort(function comparator(b, a){
+        return b.budget - a.budget;
+      })
       .value(function(d) { return d.budget; });
 
     d3.json(urlData)
@@ -58,6 +70,9 @@ var TreemapVis = Class.extend({
             return "treemap_node";
           }
         }.bind(this))
+        .attr("data-tooltip", function(d){ 
+          return "<strong>" + d.name + "</strong><br>" + accounting.formatMoney(d.budget) + "<br>" + d.budget_per_inhabitant + "€/habitante";
+        }.bind(this))
         .attr("data-url", function(d){ 
           if(this.clickable){
             return d.children ? null : urlData.split('?')[0] + "?parent_code=" + d.code;
@@ -74,7 +89,9 @@ var TreemapVis = Class.extend({
               return "<p><strong>" + d.name + "</strong></p><p>" + d.budget_per_inhabitant + "€/habitante</p>";
             }
           }
-        });
+        })
+        .on('mouseover', this._mouseover.bind(this))
+        .on('mouseout', this._mouseout.bind(this));
     }.bind(this));
   },
 
@@ -83,5 +100,32 @@ var TreemapVis = Class.extend({
       .style("top", function(d) { return d.y + "px"; })
       .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
       .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
-  }
+  },
+
+  _mouseover: function(){
+    var selected = d3.event.target;
+    if (selected.tagName != 'DIV'){
+      while (selected.tagName != 'DIV') {
+        selected = selected.parentNode;
+      }
+    }
+    var html = selected.dataset.tooltip;
+ 
+    this.tooltip
+        .transition()
+        .duration(this.duration / 4)
+        .style('opacity', this.opacity);
+
+    this.tooltip
+        .html(html)
+        .style('left', (d3.event.pageX - 75) + 'px')
+        .style('top', (d3.event.pageY - 25) + 'px');
+  },
+
+  _mouseout: function(){
+    this.tooltip.transition()
+        .duration(this.duration / 4)
+        .style("opacity", 0);
+  },
+
 });
