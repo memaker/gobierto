41c75fe460857d0c4b1033e83ae13b442c902966
Fernando Blat <ferblape@gmail.com>
2016-01-28 11:02:49 +0100

    Removed method not used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,10 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActionController::RoutingError, with: :render_404
 
-  helper_method :reset_filters_parameters, :code_from_params
+  helper_method :code_from_params
 
   def render_404
     render file: "public/404", status: 404, layout: false, handlers: [:erb], formats: [:html]
-  end
-
-  def reset_filters_parameters
-    [:location_id, :location_type, :population, :similar_budget_min, :similar_budget_max,
-     :total_similar_budget_min, :total_similar_budget_max, :code, :format]
   end
 
   def helpers
