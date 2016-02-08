abced04d89f9634c541f32c1b16a61ab28daf7c4
Fernando Blat <ferblape@gmail.com>
2016-02-08 13:21:56 +0100

    Fix places api

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,11 +1,43 @@
 class Api::PlacesController < ApplicationController
   def index
-    @places = INE::Places::Place.collection_for_search(params[:q])
+    @places = search_place(params[:q])
 
     respond_to do |format|
       format.json do
         render json: @places.to_json
       end
     end
+  end
+
+  private
+
+  def search_place(query)
+    return [] if query.blank? || query.length < 3
+
+    query = {
+      query: {
+        wildcard: {
+          name: "#{query.downcase}*"
+        }
+      },
+      size: 10_000
+    }
+
+    response = SearchEngine.client.search index: 'data', type: 'places', body: query
+    source = response['hits']['hits'].map{|h| h['_source'] }
+    source.map do |place|
+      ine_place = INE::Places::Place.find(place['ine_code'])
+      next if ine_place.nil?
+
+      {
+        value: ine_place.name,
+        data: {
+          category: ine_place.province.name,
+          id: ine_place.id,
+          slug: ine_place.slug,
+          type: 'Place'
+        }
+      }
+    end.compact
   end
 end
