5e41a8c07aa91a476f873e7c0283d1b3e640bf0b
Fernando Blat <ferblape@gmail.com>
2016-02-18 15:53:59 +0100

    Bump to Ruby 2.3.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, 'git@github.com:PopulateTools/gobierto-budgets-comparator.git'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/cache')
 set :rbenv_type, :user
-set :rbenv_ruby, '2.2.2'
+set :rbenv_ruby, '2.3.0'
 set :rbenv_path, '/home/ubuntu/.rbenv'
 
 set :passenger_restart_with_touch, true
