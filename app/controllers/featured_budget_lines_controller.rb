fc2f00bcedc7b0646a3a1b6ad14789f372da1e87
Fernando Blat <ferblape@gmail.com>
2016-04-05 18:19:02 +0200

    Avoid failure when there is no data

--- app/controllers/featured_budget_lines_controller.rb
+++ app/controllers/featured_budget_lines_controller.rb
@@ -3,20 +3,22 @@ class FeaturedBudgetLinesController < ApplicationController
     @place = INE::Places::Place.find_by_slug(params[:id])
     @year = params[:year]
     @area_name = 'functional'
-    
+
     @kind = BudgetLine::EXPENSE
     results = BudgetLine.search({
         kind: @kind, year: @year, ine_code: @place.id,
         type: @area_name, range_hash: {
-          level: {ge: 3}, 
+          level: {ge: 3},
           amount_per_inhabitant: { gt: 0 }
         }
     })['hits']
-    
-    @code = results.sample['code']
+
+    @code = results.sample['code'] if results.any?
 
     respond_to do |format|
-      format.js
+      format.js do
+        @code.present? ? render('show') : render(nothing: true)
+      end
     end
   end
 end
