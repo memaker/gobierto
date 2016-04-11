dc0680a0301fc0926d3ae7e4f4134bff80d14c05
Fernando Blat <ferblape@gmail.com>
2016-04-11 15:34:23 +0200

    Remove module

--- app/models/mailchimp.rb
+++ app/models/mailchimp.rb
@@ -1,54 +1,52 @@
-module GobiertoBudgets
-  class Mailchimp
-    LIST_URL = 'https://us11.api.mailchimp.com/3.0/lists/c5ae573572/members/'
-    API_KEY = Rails.application.secrets.mailchimp_api_key
+class Mailchimp
+  LIST_URL = 'https://us11.api.mailchimp.com/3.0/lists/c5ae573572/members/'
+  API_KEY = Rails.application.secrets.mailchimp_api_key
 
-    def self.is_member?(user)
-      email_digest = Digest::MD5.hexdigest user.email.downcase
+  def self.is_member?(user)
+    email_digest = Digest::MD5.hexdigest user.email.downcase
 
-      req = Faraday.new(url: "#{LIST_URL}#{email_digest}")
-      req.basic_auth('anystring',API_KEY)
-      response = req.get
+    req = Faraday.new(url: "#{LIST_URL}#{email_digest}")
+    req.basic_auth('anystring',API_KEY)
+    response = req.get
 
-      response.status == 200
+    response.status == 200
+  end
+
+  def self.add_member(user)
+    conn = Faraday.new(url: LIST_URL)
+    conn.basic_auth('anystring',API_KEY)
+
+    response = conn.post do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = request_body(user)
     end
+  end
 
-    def self.add_member(user)
-      conn = Faraday.new(url: LIST_URL)
-      conn.basic_auth('anystring',API_KEY)
+  def self.update_member(user)
+    email_digest = Digest::MD5.hexdigest user.email.downcase
 
-      response = conn.post do |req|
-        req.headers['Content-Type'] = 'application/json'
-        req.body = request_body(user)
-      end
+    conn = Faraday.new(url: "#{LIST_URL}#{email_digest}")
+    conn.basic_auth('anystring',API_KEY)
+
+    response = conn.put do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = request_body(user, :update)
     end
+  end
 
-    def self.update_member(user)
-      email_digest = Digest::MD5.hexdigest user.email.downcase
-
-      conn = Faraday.new(url: "#{LIST_URL}#{email_digest}")
-      conn.basic_auth('anystring',API_KEY)
-
-      response = conn.put do |req|
-        req.headers['Content-Type'] = 'application/json'
-        req.body = request_body(user, :update)
-      end
-    end
-
-    private
-    def self.request_body(user, action = :add)
-      body = {
-        "email_address": user.email,
-        "merge_fields": {
-          "FNAME": (user.first_name || ''),
-          "LNAME": (user.last_name || ''),
-          "MUNICIPIO": (user.place_id.present? ? INE::Places::Place.find(user.place_id).name : 'Sin especificar'),
-          "MMERGE4": (user.pro? ? 'Profesional de Administración Local' : 'Ciudadano')
-        }
+  private
+  def self.request_body(user, action = :add)
+    body = {
+      "email_address": user.email,
+      "merge_fields": {
+        "FNAME": (user.first_name || ''),
+        "LNAME": (user.last_name || ''),
+        "MUNICIPIO": (user.place_id.present? ? INE::Places::Place.find(user.place_id).name : 'Sin especificar'),
+        "MMERGE4": (user.pro? ? 'Profesional de Administración Local' : 'Ciudadano')
       }
+    }
 
-      body.merge!("status": "subscribed", "email_type": "html", "language": "es") if action == :add
-      body.to_json
-    end
+    body.merge!("status": "subscribed", "email_type": "html", "language": "es") if action == :add
+    body.to_json
   end
 end
