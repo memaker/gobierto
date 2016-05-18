557f01ab209abae38e92a0d233190fe5322876d8
Fernando Blat <ferblape@gmail.com>
2016-05-18 13:05:26 +0200

    Bump ruby to 2.3.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, 'git@github.com:PopulateTools/gobierto.git'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/cache')
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.0'
+set :rbenv_ruby, '2.3.1'
 set :rbenv_path, '/home/ubuntu/.rbenv'
 set :delayed_job_workers, 1
 
