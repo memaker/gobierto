87a4c1f75e6ec67aeea3ac0816b85f8cf942d4e1
Fernando Blat <ferblape@gmail.com>
2016-06-01 11:28:28 +0200

    Site stats module
    Closes #180

--- app/controllers/gobierto_sites/budgets_controller.rb
+++ app/controllers/gobierto_sites/budgets_controller.rb
@@ -5,6 +5,10 @@ class GobiertoSites::BudgetsController < GobiertoSites::ApplicationController
     @kind = GobiertoBudgets::BudgetLine::EXPENSE
     @area_name = GobiertoBudgets::BudgetLine::FUNCTIONAL
 
+    @site_stats = SiteStats.new site: @site, year: @year
+
+    @top_income_budget_lines = GobiertoBudgets::TopBudgetLine.limit(5).where(year: @year, place: @site.place, kind: GobiertoBudgets::BudgetLine::INCOME).all
+    @top_expense_budget_lines = GobiertoBudgets::TopBudgetLine.limit(5).where(year: @year, place: @site.place, kind: GobiertoBudgets::BudgetLine::INCOME).all
     @place_budget_lines = GobiertoBudgets::BudgetLine.where(place: @place, level: 1, year: @year, kind: @kind, area_name: @area_name).all
   end
 
@@ -19,7 +23,7 @@ class GobiertoSites::BudgetsController < GobiertoSites::ApplicationController
     if params[:year].nil?
       redirect_to gobierto_sites_budgets_path(GobiertoBudgets::SearchEngineConfiguration::Year.last)
     else
-      @year = params[:year]
+      @year = params[:year].to_i
     end
   end
 
---
+++ app/models/site_stats.rb
@@ -0,0 +1,66 @@
+class SiteStats
+  def initialize(options)
+    @site = options.fetch :site
+    @place = @site.place
+    @year = options.fetch(:year).to_i
+  end
+
+  def total_budget_per_inhabitant(year = nil)
+    year ||= @year
+    total_budget_planned_query(year)['_source']['total_budget_per_inhabitant'].to_f
+  end
+
+  def total_budget(year = nil)
+    year ||= @year
+    total_budget_planned_query(year)['_source']['total_budget'].to_f
+  end
+  alias_method :total_budget_planned, :total_budget
+
+  def total_budget_executed(year = nil)
+    year ||= @year
+    total_budget_executed_query(year)['_source']['total_budget']
+  end
+
+  def debt(year = nil)
+    year ||= @year
+    value = GobiertoBudgets::SearchEngine.client.get index: GobiertoBudgets::SearchEngineConfiguration::Data.index,
+      type: GobiertoBudgets::SearchEngineConfiguration::Data.type_debt, id: [@place.id, year].join('/')
+    value['_source']['value'] * 1000
+  rescue Elasticsearch::Transport::Transport::Errors::NotFound
+    nil
+  end
+
+  def percentage_difference(options)
+    year = options.fetch(:year, @year)
+    variable1 = options.fetch(:variable1)
+    variable2 = options.fetch(:variable2, options.fetch(:variable1))
+    diff = if variable1 == variable2
+      year1 = options.fetch(:year1)
+      year2 = options.fetch(:year2)
+
+      ((self.send(variable1, year1).to_f - self.send(variable1, year2).to_f)/self.send(variable1, year2).to_f) * 100
+    else
+      ((self.send(variable1, year).to_f - self.send(variable2, year).to_f)/self.send(variable2, year).to_f) * 100
+    end
+
+    direction = (diff < 0) ? 'más' : 'menos'
+
+    "#{ActionController::Base.helpers.number_with_precision(diff, precision: 2)}% #{direction}"
+  end
+
+  private
+
+  def total_budget_planned_query(year)
+    GobiertoBudgets::SearchEngine.client.get index: GobiertoBudgets::SearchEngineConfiguration::TotalBudget.index_forecast,
+      type: GobiertoBudgets::SearchEngineConfiguration::TotalBudget.type, id: [@place.id, year].join('/')
+  rescue Elasticsearch::Transport::Transport::Errors::NotFound
+    nil
+  end
+
+  def total_budget_executed_query(year)
+    GobiertoBudgets::SearchEngine.client.get index: GobiertoBudgets::SearchEngineConfiguration::TotalBudget.index_executed,
+      type: GobiertoBudgets::SearchEngineConfiguration::TotalBudget.type, id: [@place.id, year].join('/')
+  rescue Elasticsearch::Transport::Transport::Errors::NotFound
+    nil
+  end
+end
