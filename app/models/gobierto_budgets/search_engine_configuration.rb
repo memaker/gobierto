03d71447feaf2cdf6b4c902190bb41c3021428f6
Fernando Blat <ferblape@gmail.com>
2016-09-09 09:50:07 +0200

    Use v3 of data

--- app/models/gobierto_budgets/search_engine_configuration.rb
+++ app/models/gobierto_budgets/search_engine_configuration.rb
@@ -14,14 +14,14 @@ module GobiertoBudgets
     end
 
     class BudgetLine
-      def self.index_forecast; 'budgets-forecast-v2' end
-      def self.index_executed; 'budgets-execution-v2' end
+      def self.index_forecast; 'budgets-forecast-v3' end
+      def self.index_executed; 'budgets-execution-v3' end
       # TODO: add the types economic and functional
     end
 
     class TotalBudget
-      def self.index_forecast; 'budgets-forecast-v2' end
-      def self.index_executed; 'budgets-execution-v2' end
+      def self.index_forecast; 'budgets-forecast-v3' end
+      def self.index_executed; 'budgets-execution-v3' end
       def self.type; 'total-budget' end
     end
 
